7bf731bf6deec37af6985552e07c5f990f5f7057
Arnob Tanjim <arnob.tanjim@gmail.com>
2019-03-04 23:55:13 +0600

    splash screen code updated

--- Project Code/app/src/main/java/com/masterT/rsr/splashScreen.java
+++ Project Code/app/src/main/java/com/masterT/rsr/splashScreen.java
@@ -1,13 +1,25 @@
 package com.masterT.rsr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class splashScreen extends AppCompatActivity {
+public class tavelbuddy extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+        
+
+
+    }
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+
     }
 }
