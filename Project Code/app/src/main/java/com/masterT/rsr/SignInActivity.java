879ef056335b6416ca92929bc3a57f9d14a4cbd9
RashadTanjim <rashad.tanjim@northsouth.edu>
2019-03-11 21:12:56 +0600

    updates activities & marging problems solved!
    Co-Authored-By: MD. Rashad Tanjim <rashadtanjim@users.noreply.github.com>

---
+++ Project Code/app/src/main/java/com/masterT/rsr/SignInActivity.java
@@ -0,0 +1,4 @@
+package com.masterT.rsr;
+
+public class SignInActivity {
+}
--- Project Code/app/src/main/java/com/masterT/rsr/SplashScreen.java
+++ Project Code/app/src/main/java/com/masterT/rsr/SplashScreen.java
@@ -3,7 +3,7 @@ package com.masterT.rsr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class splashScreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
