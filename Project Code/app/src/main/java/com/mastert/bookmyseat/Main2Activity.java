1d51a53a5dd8ca2a5931f96f19cffae14f4bb872
sanzar <sanzar.rahman@northsouth.edu>
2019-03-19 01:26:29 +0600

    Update Main2Activity.java

--- Project Code/app/src/main/java/com/mastert/bookmyseat/Main2Activity.java
+++ Project Code/app/src/main/java/com/mastert/bookmyseat/Main2Activity.java
@@ -1,5 +1,7 @@
 package com.mastert.bookmyseat;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 public class Main2Activity extends AppCompatActivity {
+    private static final int CHOOSE_IMAGE = 101 ;
     ImageView imageView;
     EditText editText;
 
@@ -20,14 +23,31 @@ public class Main2Activity extends AppCompatActivity {
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                showImageChooser();
             }
         });
         findViewById(R.id.setpic).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==CHOOSE_IMAGE && requestCode==RESULT_OK && data!=null && data.getData()!=null )
+        {
+            data.getData();
+        }
+
+    }
+
+    private void showImageChooser(){
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Set profile picture"),CHOOSE_IMAGE);
     }
 }
