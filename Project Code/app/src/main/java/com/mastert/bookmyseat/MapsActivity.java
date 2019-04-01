c5f9c1b499145b9206527af675bf1172f665290d
RashadTanjim <rashad.tanjim@northsouth.edu>
2019-04-02 01:26:04 +0600

    navigation bar icon folder updated!

--- Project Code/app/src/main/java/com/mastert/bookmyseat/MapsActivity.java
+++ Project Code/app/src/main/java/com/mastert/bookmyseat/MapsActivity.java
@@ -12,7 +12,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 //Our Map API key is AIzaSyBvC_BNOjfNmfa_DlXf1EGfynifEa-vBG0
 //This key is only verified for our app BookMySeat
-//Mail:  travel.buddy.finder@gmail.com   
+//Mail:  travel.buddy.finder@gmail.com
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
