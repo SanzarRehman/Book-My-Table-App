887ecc9b9fe177c0f3561c4cacb9b7c13d99ea2a
RashadTanjim <rashad.tanjim@northsouth.edu>
2019-04-02 02:13:58 +0600

    debug map activity error!

--- Project Code/app/src/main/java/com/mastert/bookmyseat/MapsActivity.java
+++ Project Code/app/src/main/java/com/mastert/bookmyseat/MapsActivity.java
@@ -1,58 +1,38 @@
 package com.mastert.bookmyseat;
 
-
-
 //Our Map API key is AIzaSyBvC_BNOjfNmfa_DlXf1EGfynifEa-vBG0
 //This key is only verified for our app BookMySeat
 //Mail:  travel.buddy.finder@gmail.com
 
+import android.*;
 import android.Manifest;
-import android.annotation.SuppressLint;
-
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity  implements  OnMapReadyCallback {
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     LocationManager locationManager;
 
     LocationListener locationListener;
-
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -77,24 +57,28 @@ public class MapsActivity extends AppCompatActivity  implements  OnMapReadyCallb
 
     }
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
 
-    @SuppressLint("MissingPermission")
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
         mMap = googleMap;
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -106,15 +90,10 @@ public class MapsActivity extends AppCompatActivity  implements  OnMapReadyCallb
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
 
                 mMap.clear();
-                mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location")
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
+                mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 17));
 
-                LatLng nsu = new LatLng(23.8151152, 90.4249748); //@23.8151152,90.4249748 (NSU Location)
-                mMap.addMarker(new MarkerOptions().position(nsu).title("North South University")
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(nsu));
+
             }
 
             @Override
@@ -152,8 +131,7 @@ public class MapsActivity extends AppCompatActivity  implements  OnMapReadyCallb
                 LatLng userLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
                 mMap.clear();
 
-                mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location")
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
+                mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
 
 
@@ -161,6 +139,7 @@ public class MapsActivity extends AppCompatActivity  implements  OnMapReadyCallb
 
 
         }
+
 
     }
 }
