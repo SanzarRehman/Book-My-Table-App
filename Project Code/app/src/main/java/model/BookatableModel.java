acc808f15b7a241a7d1ef6928507725d100a01b1
RashadTanjim <rashad.tanjim@northsouth.edu>
2019-04-16 02:07:37 +0600

    model activitis added

---
+++ Project Code/app/src/main/java/model/BookatableModel.java
@@ -0,0 +1,19 @@
+package model;
+
+
+
+public class BookatableModel {
+     String txttime;
+
+    public String getTxttime() {
+        return txttime;
+    }
+
+    public void setTxttime(String txttime) {
+        this.txttime = txttime;
+    }
+
+    public BookatableModel(String txttime) {
+        this.txttime = txttime;
+    }
+}
---
+++ Project Code/app/src/main/java/model/ListModel.java
@@ -0,0 +1,19 @@
+package model;
+
+
+
+public class ListModel {
+    String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ListModel(String title) {
+        this.title = title;
+    }
+}
