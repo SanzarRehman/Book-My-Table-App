953928dacf8cf70006140cb9167bb1926b3aa8d8
RashadTanjim <rashad.tanjim@northsouth.edu>
2019-04-30 04:15:18 +0600

    fab bar updated!

--- Project Code/app/src/main/java/com/mastert/bookmyseat/NavBar.java
+++ Project Code/app/src/main/java/com/mastert/bookmyseat/NavBar.java
@@ -28,8 +28,8 @@ public class NavBar extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Send mail for any query!", Snackbar.LENGTH_LONG)
+                        .setAction("seat.my.book@gmail.com", null).show();
             }
         });
 
